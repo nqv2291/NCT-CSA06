a11517ba1158a73376089478664cc6ba81754243
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2022-09-30 23:11:23 +0700

    Update onetoten.sql

--- Labs/Coding Exercises/Lab06/Uong_Minh_Duc/onetoten.sql
+++ Labs/Coding Exercises/Lab06/Uong_Minh_Duc/onetoten.sql
@@ -1,1 +1,21 @@
+USE NorthwindFullData
+GO
+
+SELECT * FROM Customers
+WHERE Country = 'Mexico'
+
+SELECT DISTINCT Country
+FROM Customers
+
+SELECT * FROM Customers
+WHERE Country = ('Mexico', 'Canada', ' USA')
+
+SELECT CustomerName FROM Customer
+WHERE Country = 'USA' AND Country != 'San Francisco'
+
+SELECT ProductName AS 'Name', Unit 
+FROM Products
+
+ORDER BY Price ASC
+
 
