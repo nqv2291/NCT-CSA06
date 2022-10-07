7e3082bb9e0396b81aa72975dade8ee25787dcc9
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2022-10-07 20:30:37 +0700

    Create onetoten.sql

---
+++ Labs/Coding Exercises/Lab07/Uong_Minh_Duc/onetoten.sql
@@ -0,0 +1,34 @@
+SELECT MIN(OrderDate) AS EarliestDate
+FROM Orders
+
+SELECT COUNT(OrderID) AS TotalOrders 
+FROM Orders
+
+SELECT OrderID, SUM(Quantity) AS TotalItems 
+FROM OrderDetails 
+GROUP BY OrderID
+
+SELECT SUM(Quantity) *1/COUNT(DISTINCT OrderID) AS AvgItems 
+FROM OrderDetails
+
+SELECT OrderID, COUNT(Quantity) AS DistinctItems
+From OrderDetails
+GROUP BY OrderID
+
+SELECT Category, AVG(Price) AS Avgprice
+FROM Products
+GROUP BY CATEGORY
+HAVING AVG(Price)=>20 AND AVG(Price)<=30
+
+SELECT Country, COUNT(CustomerID) AS CustomerCount 
+FROM Customers 
+GROUP BY Country 
+HAVING COUNT(CustomerID)>10
+
+SELECT CustomerID, COUNT(OrderID) AS Ordered
+FROM Orders
+GROUP BY CustomerID
+ORDER BY COUNT(OrderID) DESC
+
+
+
