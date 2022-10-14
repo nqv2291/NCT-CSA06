2da75a6bfa06d2b522a29e459d754304749c32aa
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2022-10-14 21:47:03 +0700

    Update onetonine.sql

--- Labs/Coding Exercises/Lab08/Uong_Minh_Duc/onetonine.sql
+++ Labs/Coding Exercises/Lab08/Uong_Minh_Duc/onetonine.sql
@@ -1,1 +1,39 @@
+SELECT DISTINCT Orders.OrderID, Customers.CustomerID, Customers.CustomerName, Customers.Country
+FROM Orders 
+JOIN Customers ON Orders.CustomerID=Customers.CustomerID
+ORDER BY Orders.OrderID ASC
 
+SELECT DISTINCT Customers.Country, COUNT(Orders.OrderID)
+FROM Customers 
+JOIN Orders ON Orders.CustomerID=Customers.CustomerID
+GROUP BY Customers.Country
+ORDER BY COUNT(Orders.OrderID)
+
+SELECT DISTINCT Products.ProductID, Products.ProductName, OrderDetails.Quantity, Products.Price 
+FROM Products
+JOIN OrderDetails ON Products.ProductID = OrderDetails.ProductID
+WHERE OrderDetails.OrderID=10248
+
+SELECT DISTINCT OrderDetails.OrderID, SUM(OrderDetails.Quantity * Products.Price) AS TotalAmount
+FROM Products 
+JOIN OrderDetails ON Products.ProductID = OrderDetails.ProductID
+GROUP BY OrderDetails.OrderID 
+HAVING OrderDetails.OrderID=10248
+
+SELECT DISTINCT OrderDetails.OrderID, SUM(OrderDetails.Quantity * Products.Price) AS TotalAmount
+FROM Products
+JOIN OrderDetails ON Products.ProductID=OrderDetails.ProductID
+WHERE OrderDetails.OrderID IN (SELECT OrderID FROM Orders WHERE OrderDate='1996-08-01')
+GROUP BY OrderDetails.OrderID
+
+SELECT DISTINCT OrderDetails.OrderID, Customers.CustomerID, Orders.OrderDate
+FROM Products 
+INNER JOIN OrderDetails ON Products.ProductID = OrderDetails.ProductID INNER JOIN Orders ON OrderDetails.OrderID = Orders.OrderID INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+WHERE Orders.OrderDate<'1996-07-08' OR Customers.Country='Argentina'
+
+SELECT DISTINCT ProductID, ProductName, Price 
+FROM Products
+WHERE Price > (SELECT AVG(Price) FROM Products)
+ORDER BY Price ASC
+
+check12356
